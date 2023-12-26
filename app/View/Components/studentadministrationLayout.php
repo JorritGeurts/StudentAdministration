e815f753d2bd9525d580bc1a60a3589c06a2a139
Jorrit Geurts <r0956231@student.thomasmore.be>
2023-12-26 12:18:05 +0100

    weg met deze zooi

--- app/View/Components/studentadministrationLayout.php
+++ app/View/Components/studentadministrationLayout.php
@@ -21,6 +21,6 @@ class studentadministrationLayout extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.studentadministration-layout');
+        return view('layouts.studentadministration');
     }
 }
